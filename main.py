8131dfa5c20153db4ff85aaf764e8a087a0b4075
adamfares220 <adamfares220@gmail.com>
2026-01-01 17:29:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+import pandas as pd
+
+df = pd.read_csv("parkinsons.csv")
+df = df.dropna()from sklearn.preprocessing import MinMaxScaler
+
+print(df.columns.to_list())
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.pairplot(df, hue="name", diag_kind="MDVP:Fo(Hz)", corner=True)
+plt.show()
+
+selected_features = ['status', 'HNR']
+x = df[selected_features]
+y = df['PPE']
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+# x now contains only numerical columns, so we can directly apply the scaler.
+x = scaler.fit_transform(x)
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+from sklearn.ensemble import RandomForestRegressor
+model = RandomForestRegressor()
+model.fit(x_train, y_train)
+
+from sklearn.metrics import r2_score
+
+y_pred = model.predict(x_test)
+r2 = r2_score(y_test, y_pred)
+print(f'R2 Score: {r2}')
+
+import joblib
+
+joblib.dump(model, 'my_model.joblib')
 
